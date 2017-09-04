72f45e364a7a76b1103ce33cdb0333b667471f71
Juan Mendez <juanmndz19@gmail.com>
2017-09-03 19:42:10 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Tab from './components/Tab';
+import Header from './components/Header'
+import Container from './components/styles/Container'
+
+const App = () => (
+  <div>
+  <Container>
+    <Header />
+  </Container>
+    <Tab />
+  </div>
+)
+export default App;
